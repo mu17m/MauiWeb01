c8883b11ce2802209ef4bc2ae24080614a82e5bf
Hussein <Hussein@DESKTOP-RKEQNQV>
2024-12-01 15:42:22 +0300

    Add project files.

---
+++ MauiWeb/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using MauiWeb.Data;
+using MauiWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MauiWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _Db;
+        public CategoryController(ApplicationDbContext Db)
+        {
+            _Db = Db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _Db.categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
