1f345e3fa7117c53b6a6efceb7ff05e0c1dfe52a
Mostafa Mohamad <mostafamohm2002ead@gmail.com>
2024-12-11 15:12:17 +0300

    push one to one

---
+++ Maui.DataAccess/Repositry/IRepositry/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBook.DataAccess.Repositry.IRepositry
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepositry categoryRepositry { get; }
+        void Save();
+    }
+}
