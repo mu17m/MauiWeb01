3ca7e45732b257bca94310a8a338efb661d52149
Mostafa Mohamad <mostafamohm2002ead@gmail.com>
2025-01-15 21:52:11 +0300

    in user managment, add Lock Unlock button and Permission button to change the role of user

--- MauiWeb/Areas/Admin/Controllers/UserController.cs
+++ MauiWeb/Areas/Admin/Controllers/UserController.cs
@@ -1,8 +1,11 @@
 ﻿using MauiBook.DataAccess.Data;
 using MauiBook.Models;
+using MauiBook.Models.ViewModels;
 using MauiBook.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace MauiBookWeb.Areas.Admin.Controllers
@@ -12,15 +15,76 @@ namespace MauiBookWeb.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly ApplicationDbContext _Db;
-
-        public UserController(ApplicationDbContext Db)
+        private readonly UserManager<IdentityUser> _UserManager;
+        public UserController(ApplicationDbContext Db, UserManager<IdentityUser> userManager)
         {
             _Db = Db;
+            _UserManager = userManager;
         }
         public IActionResult Index()
         {
             return View();
         }
+        
+        public IActionResult RoleManagment(string userId)
+        {
+            var RoleId = _Db.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+
+            RoleManagmentVM RoleVM = new RoleManagmentVM()
+            {
+                User = _Db.ApplicationUsers.Include(u => u.company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _Db.Roles.Select(r => new SelectListItem
+                {
+                    Text = r.Name,
+                    Value = r.Name
+                }),
+                CompanyList = _Db.companies.Select(c => new SelectListItem
+                {
+                    Text = c.Name,
+                    Value = c.Id.ToString()
+                })
+            };
+            if(RoleVM.User != null)
+            {
+                RoleVM.User.Role = _Db.Roles.FirstOrDefault(r => r.Id == RoleId).Name;
+            }
+
+
+            return View(RoleVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagment(RoleManagmentVM RoleVM)
+        {
+            var RoleId = _Db.UserRoles.FirstOrDefault(r => r.UserId == RoleVM.User.Id).RoleId;
+            var OldRole = _Db.Roles.FirstOrDefault(r => r.Id == RoleId).Name;
+            ApplicationUser User = _Db.ApplicationUsers.FirstOrDefault(u => u.Id == RoleVM.User.Id);
+
+            if(!(RoleVM.User.Role == OldRole))
+            {
+                //Role was updated
+                if (RoleVM.User.Role == SD.Role_Company)
+                {
+                    User.CompanyId = RoleVM.User.CompanyId;
+                }
+                if (OldRole == SD.Role_Company)
+                {
+                    User.CompanyId = null;
+                }
+            }
+            else
+            {
+                if (RoleVM.User.Role == SD.Role_Company)
+                {
+                    User.CompanyId = RoleVM.User.CompanyId;
+                }
+            }
+                _Db.SaveChanges();
+                _UserManager.RemoveFromRoleAsync(User, OldRole).GetAwaiter().GetResult();
+                _UserManager.AddToRoleAsync(User, RoleVM.User.Role).GetAwaiter().GetResult();
+        return RedirectToAction("Index");
+        }
+
 
 
         #region API calls
@@ -46,7 +110,7 @@ namespace MauiBookWeb.Areas.Admin.Controllers
             return Json(new { data = ListOfUsers });
         }
 
-        [HttpDelete]
+        [HttpPost]
         public IActionResult LockUnlock([FromBody] string id)
         {
             var user = _Db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
@@ -67,7 +131,7 @@ namespace MauiBookWeb.Areas.Admin.Controllers
                 user.LockoutEnd = DateTime.Now.AddYears(1000);
             }
             _Db.SaveChanges();
-            return Json(new { success = true, message = "Deleted Successfully" });
+            return Json(new { success = true});
         }
 
         #endregion
--- MauiWeb/wwwroot/js/User.js
+++ MauiWeb/wwwroot/js/User.js
@@ -12,16 +12,55 @@ function loadDataTable() {
                 { data: 'email', "width": "15%" },
                 { data: 'phoneNumber', "width": "15%" },
                 { data: 'company.name', "width": "15%" },
-                { data: 'role', "width": "15%" },
+                { data: 'role', "width": "10%" },
                 {
-                    data: 'id',
+                    data: {id:"id", lockoutEnd:"lockoutEnd"},
                     "render": function (data) {
-                        return `<div class="w-75 btn-group" role="group">
-                                  <a href="/admin/user/edit?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
-                                 </div>`
+                        var today = new Date().getTime();
+                        var lockoutEnd = new Date(data.lockoutEnd).getTime();
+                        if (lockoutEnd > today) {
+                            return `
+                            <div class="text-center">
+                                <a onclick=Lockunlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
+                                    <i class="bi bi-lock-fill"></i> Locked
+                                </a>
+                                <a href="/admin/user/roleManagment?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
+                                    <i class="bi bi-pencil-square"></i> Permission
+                                </a>
+                            </div>
+                            
+                            `
+                        }
+                        else {
+                            return `
+                            <div class="text-center">
+                                <a onclick=Lockunlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                                    <i class="bi bi-unlock-fill"></i> Unlock
+                                </a>
+                                <a href="/admin/user/roleManagment?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
+                                    <i class="bi bi-pencil-square"></i> Permission
+                                </a>
+                            </div>
+                            
+                            `
+                        }
+                        
                     },
-                    "width": "25%"
+                    "width": "30%"
                 },
             ]
     });
+}
+function Lockunlock(id) {
+    $.ajax({
+        type: "POST",
+        url: '/admin/user/lockunlock',
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                DataTable.ajax.reload();
+            }
+        }
+    })
 }
